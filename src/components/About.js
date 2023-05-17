d36790b70209e46a80430497de0769ea20b1ebd3
VrajParekh <vrajparekh58@gmail.com>
2023-05-17 12:21:41 +0530

    about page changed

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div>Hello i am About page</div>;
+};
 
-export default About
+export default About;
